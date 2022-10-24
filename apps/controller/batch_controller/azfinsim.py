ca29a5b3493d172fd2e12d7fe517e403f3bdcefc
Utkarsh Ayachit <uayachit@microsoft.com>
2022-10-24 18:22:48 -0400

    update python code

--- apps/controller/batch_controller/azfinsim.py
+++ apps/controller/batch_controller/azfinsim.py
@@ -2,47 +2,98 @@ import argparse
 
 from . import utils
 
+def get_parser():
+    parser = argparse.ArgumentParser(description='FinTech Risk Simulator')
 
-def add_arguments(parser: argparse.ArgumentParser) -> None:
-    g = parser.add_mutually_exclusive_group(required=True)
-    g.add_argument('-r','--resize-pool', type=int, help='resize pool',metavar='SIZE')
-    g.add_argument('-j','--submit-job', action='store_true')
+    parser.add_argument('-e', '--batch-endpoint',
+        type=str, help='batch account endpoint', required=True)
 
-    sg = parser.add_argument_group('submit-job', 'arguments for job submission')
-    sg.add_argument('-s','--start-trade', type=int, help='start trade number', default=0)
-    sg.add_argument('-w','--trade-window', type=int, help='trade window i.e. total number of trades', default=0)
-    sg.add_argument('-t','--tasks', type=int, help='total number of tasks', default=1)
-    sg.add_argument('-i','--container-image',type=str, help='container image name', default='')
-    sg.add_argument('-a','--algorithm', choices=['deltavega', 'pvonly', 'synthetic'], default='deltavega', help='pricing algorithm')
-    sg.add_argument("--failure", type=float, default=0.0, help="inject random task failure with this probability (default: 0.0)")
+    subparsers = parser.add_subparsers(title='command', description='valid commands')
 
-    tg = parser.add_argument_group('synthetic', 'arguments with algorithm=\'synthetic\'')
+    poolParser = subparsers.add_parser('pool', help='pool operations')
+    poolParser.add_argument('-i', '--info', action='store_true', help='print pool information')
+    poolParser.add_argument('-r','--resize', type=int, help='resize pool', metavar='SIZE', default=-1)
+    poolParser.set_defaults(command_execute=execute_pool)
+
+    jobParser = subparsers.add_parser('job', help='job operations')
+    jobParser.add_argument('-c','--container-registry-name',type=str, help='container registry url [REQUIRED]', required=True)
+    jobParser.add_argument('-s','--start-trade', type=int, help='start trade number', default=0)
+    jobParser.add_argument('-w','--trade-window', type=int, help='trade window i.e. total number of trades', default=0)
+    jobParser.add_argument('-t','--tasks', type=int, help='total number of tasks', default=1)
+    jobParser.add_argument('-a','--algorithm', choices=['deltavega', 'pvonly', 'synthetic'], default='deltavega', help='pricing algorithm')
+    jobParser.add_argument("--failure", type=float, default=0.0, help="inject random task failure with this probability (default: 0.0)")
+
+    tg = jobParser.add_argument_group('synthetic', 'arguments with algorithm=\'synthetic\'')
     tg.add_argument("-d", "--delay-start", type=int, default=0, help="delay startup time in seconds (default: 0)")
     tg.add_argument("-m", "--mem-usage", type=int, default=16, help="memory usage for task in MB (default: 16)")
     tg.add_argument("--task-duration", type=int, default=20, help="task duration in milliseconds (default: 20)")
 
+    jobParser.set_defaults(command_execute=execute_job)
 
-command = '/bin/sh -c "python3 -m azfinsim --start-trade {start} --trade-window {delta} --failure {failure} --algorithm {algorithm} ' \
-          '--delay-start {delay_start} --mem-usage {mem_usage} --task-duration {task_duration} --arguments $AZ_BATCH_JOB_PREP_WORKING_DIR/azfinsim.$AZ_BATCH_JOB_ID.args"'
+    cacheParser = subparsers.add_parser('cache', help='cache operations')
+    cacheParser.add_argument('-c','--container-registry-name',type=str, help='container registry url [REQUIRED]', required=True)
+    cacheParser.add_argument('-s','--start-trade', type=int, help='start trade number', default=0)
+    cacheParser.add_argument('-w','--trade-window', type=int, help='trade window i.e. total number of trades', default=0)
+    cacheParser.add_argument('-t','--tasks', type=int, help='total number of tasks', default=1)
+    cacheParser.set_defaults(command_execute=execute_cache)
+    return parser
 
-def task_commandline_generator(args):
+def task_command_line_generator(args):
+    command = '-m azfinsim.azfinsim --config /opt/secrets/config.json --start-trade {start} --trade-window {delta} --failure {failure} --algorithm {algorithm}'
+    command_synthetic = ' --delay-start {delay_start} --mem-usage {mem_usage} --task-duration {task_duration}'
+
     delta = args.trade_window // args.tasks
     assert delta >= 0
 
     counter = 0
     for start in range(args.start_trade, args.start_trade + args.trade_window,  delta):
-        yield command.format(start=start, delta=delta, algorithm=args.algorithm,
-            failure=args.failure, delay_start=args.delay_start, mem_usage=args.mem_usage, task_duration=args.task_duration)
+        cmd = command.format(start=start, delta=delta, algorithm=args.algorithm,
+            failure=args.failure)
+        if args.algorithm == 'synthetic':
+            cmd + command_synthetic.format(delay_start=args.delay_start, mem_usage=args.mem_usage, task_duration=args.task_duration)
+        yield cmd
         counter += 1
 
-def execute(args)->None:
-    if args.resize_pool is not None:
-        utils.pool_resize(endpoint=args.batch_endpoint, pool_id='azfinsim-pool', targetSize=args.resize_pool)
+def populate_command_line_generator(args):
+    command = '-m azfinsim.generator --config /opt/secrets/config.json --start-trade {start} --trade-window {delta}'
+
+    delta = args.trade_window // args.tasks
+    assert delta >= 0
+
+    counter = 0
+    for start in range(args.start_trade, args.start_trade + args.trade_window,  delta):
+        cmd = command.format(start=start, delta=delta)
+        yield cmd
+        counter += 1
+
+
+def execute_pool(args)->None:
+    if args.resize >= 0:
+        utils.pool_resize(endpoint=args.batch_endpoint, pool_id='azfinsim-pool', targetSize=args.resize)
     else:
-        # utils.submit_job(endpoint=args.batch_endpoint, pool_id='azfinsim-pool',
-        #     num_tasks=args.tasks, tasks=task_generator(args),
-        #     task_container_image=args.container_image,
-        # )
-        tasks =[ i for i in task_commandline_generator(args)]
-        print(tasks)
-        
+        utils.print_pool_info(endpoint=args.batch_endpoint, pool_id='azfinsim-pool')
+
+def execute_job(args)->None:
+    utils.submit_job(endpoint=args.batch_endpoint, pool_id='azfinsim-pool',
+        num_tasks=args.tasks, task_command_lines=task_command_line_generator(args),
+        task_container_image='{}.azurecr.io/azfinsim/azfinsim:latest'.format(args.container_registry_name),
+        container_run_options='-v /opt/azfinsim-secrets:/opt/secrets',
+        job_id_prefix='azfinsim')
+
+
+def execute_cache(args)->None:
+    utils.submit_job(endpoint=args.batch_endpoint, pool_id='azfinsim-pool',
+        num_tasks=args.tasks, task_command_lines=populate_command_line_generator(args),
+        task_container_image='{}.azurecr.io/azfinsim/azfinsim:latest'.format(args.container_registry_name),
+        container_run_options='-v /opt/azfinsim-secrets:/opt/secrets',
+        job_id_prefix='cache')
+
+def execute(args)->None:
+    if hasattr(args, 'command_execute'):
+        args.command_execute(args)
+    else:
+        print('missing required command')
+
+if __name__ == '__main__':
+    parser = get_parser()
+    execute(parser.parse_args())
--- apps/controller/batch_controller/utils.py
+++ apps/controller/batch_controller/utils.py
@@ -10,7 +10,50 @@ def login(endpoint):
     batch_client = BatchServiceClient(wrapper, batch_url="https://{}".format(endpoint))
     return batch_client
 
+def unique_id():
+    import time
+    return time.strftime("%Y%m%d-%H%M%S")
+
 def pool_resize(endpoint, pool_id, targetSize):
     """Resize a pool"""
     client = login(endpoint)
     client.pool.resize(pool_id, models.PoolResizeParameter(target_dedicated_nodes=targetSize))
+
+def print_pool_info(endpoint, pool_id):
+    """Print information about a pool"""
+    client = login(endpoint)
+    info = client.pool.get(pool_id=pool_id)
+    print("""=============================================
+{id} (display name: '{display_name}')
+=============================================
+State: {state} (allocation state: {allocation_state})
+Current Dedicated Size: {size}
+Current Spot Size: {spot_size}
+""".format(id=info.id, 
+    display_name=info.display_name if info.display_name else '<n/a>',
+    state=info.state, size=info.current_dedicated_nodes,
+    spot_size=info.current_low_priority_nodes,
+    allocation_state=info.allocation_state))
+
+def submit_job(endpoint, pool_id, num_tasks, task_command_lines, task_container_image=None,
+    container_run_options=None, job_id_prefix='job'):
+    """submit a new job"""
+    client = login(endpoint)
+    job_id = "{}-{}".format(job_id_prefix, unique_id())
+
+    pool_info=models.PoolInformation(pool_id=pool_id)
+    client.job.add(models.JobAddParameter(id=job_id, pool_info=pool_info))
+
+    task_container_settings = models.TaskContainerSettings(image_name=task_container_image,
+        container_run_options=container_run_options) if task_container_image else None
+    tasks = [models.TaskAddParameter(id="task_{}".format(index),
+                command_line=cmd,
+                container_settings=task_container_settings) for index, cmd in enumerate(task_command_lines)]
+    res = client.task.add_collection(job_id, tasks)
+    # print(res)
+
+    # once tasks are added to job, update the job to terminate the job
+    # once all tasks complete
+    client.job.update(job_id=job_id,
+        job_update_parameter=models.JobUpdateParameter(on_all_tasks_complete='terminateJob',
+        pool_info=pool_info))
